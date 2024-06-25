7634b544eb17e472a52572f792fd48afde0f28b9
bmkedge <balamurali@kedge360.com>
2024-06-25 13:25:09 +0530

    Agent updated

--- src/agent.py
+++ src/agent.py
@@ -1,14 +1,10 @@
-import os 
 import sys
 from src.logger import logging
-from dotenv import load_dotenv
-from langchain_groq import ChatGroq
 from src.prompt import analyse_prompt
 from src.exception import StockAnalyserException
 from langchain.agents import AgentExecutor, create_tool_calling_agent
 from src.tools import get_financialstatements_tool, get_stockprice_tool, newssearch_tool, tickersearch_tool, chat
 
-load_dotenv()
 
 def create_stock_analyser_agent():
 
